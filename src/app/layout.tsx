0ea3359bb161331bb238f2e9362167091af1f73b
rudsi <onlypersonwhoknowsthis@gmail.com>
2024-02-06 15:01:21 +0530

    fixed the modal error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 "use client";
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from "@/components/Layout/Layout";
-import theme from "@/chakra/theme";
 import AuthModal from '@/components/Modal/Auth/AuthModal';
+import theme from "@/chakra/theme";
 import { RecoilRoot } from 'recoil';
 
 export default  function RootLayout({ children }: { children: React.ReactNode }) {
    
   return (
     <html lang="en">
-      <RecoilRoot>
+    <RecoilRoot>
         <ChakraProvider theme={theme}>
           <Layout>
               <div>{children}</div>
           </Layout>
         </ChakraProvider>
-      </RecoilRoot>
-      </html>
+    </RecoilRoot>
+    </html>
   );
 }
